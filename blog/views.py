094e3f0c4f419b56e7037a8b72c0d693413ef49e
abolfazl <eshagh.abolfazl@gmail.com>
2024-05-06 23:54:07 +0330

    in blog models Post implemented Post registered in blog.admin.py initial database implemented

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog post 1',
-        'date': 'may 12, 2022',
-        'content': 'first post content'
-    }
-    ,
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'date': 'may 13, 2022',
-        'content': 'second post content'
-    }
-]
+# posts = [
+#     {
+#         'author': 'John Doe',
+#         'title': 'Blog post 1',
+#         'date': 'may 12, 2022',
+#         'content': 'first post content'
+#     }
+#     ,
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog post 2',
+#         'date': 'may 13, 2022',
+#         'content': 'second post content'
+#     }
+# ]
 
 def home(request):
-    context ={'posts': posts}
+    context ={'posts': Post.objects.all()}
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, "blog/home.html", context)
 
